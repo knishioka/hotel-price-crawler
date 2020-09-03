541039951dae6d7e2de54f2a8bb2353ec185e61d
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2020-09-03 23:01:12 +0900

    Use wait instead of sleep

--- crawler.py
+++ crawler.py
@@ -2,13 +2,15 @@
 import argparse
 import datetime
 import logging
-import random
 from time import sleep
 
 from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 logger = logging.getLogger("Hotel Crawling")
 logger.setLevel(logging.DEBUG)
@@ -28,24 +30,23 @@ def scrape_hotels(no_of_rooms, no_of_nights, no_of_adults, no_of_children):
     logger.info("User typing the input fields as per Requirement....")
 
     driver = set_driver()
+    wait = WebDriverWait(driver, 10)
 
     logger.debug("Executing driver to website....")
     driver.get("https://www.marriott.com/search/default.mi")
     driver.maximize_window()
 
-    sleeping_time = random.randint(3, 7)
-    sleep(sleeping_time)
-
     logger.info("Filling the input fields in website as per Requirement....")
-    driver.find_element_by_xpath("//input[@name='destinationAddress.destination']").send_keys("Japan")
-    sleep(1)
 
-    check_in = driver.find_element_by_xpath("//input[@placeholder='Check-in']")
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@name='destinationAddress.destination']"))).send_keys(
+        "Japan"
+    )
+
+    check_in = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Check-in']")))
     for i in range(12):
         check_in.send_keys(Keys.BACKSPACE)
-    sleep(1)
 
-    flexible_date = driver.find_element_by_xpath("//span[@data-tab-value='Flexible Dates']")
+    flexible_date = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[@data-tab-value='Flexible Dates']")))
     driver.execute_script("arguments[0].click();", flexible_date)
     try:
         nights = driver.find_element_by_xpath("//a[@aria-label='Add Nights']")
@@ -53,29 +54,28 @@ def scrape_hotels(no_of_rooms, no_of_nights, no_of_adults, no_of_children):
             for _ in range(no_of_nights - 1):
                 sleep(0.1)
                 driver.execute_script("arguments[0].click();", nights)
-        sleep(1)
-        done_button = driver.find_element_by_xpath("//div[contains(@class,'flex-done-button')]")
+
+        done_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@class,'flex-done-button')]")))
         driver.execute_script("arguments[0].click();", done_button)
     except Exception:
         print("Kindly enter the No. of Nights in correct format i.e. 15")
         driver.close()
         return
 
-    sleep(1)
-    edit_rooms = driver.find_element_by_xpath("//a[@aria-label='Rooms & Guests']")
+    edit_rooms = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@aria-label='Rooms & Guests']")))
     driver.execute_script("arguments[0].click();", edit_rooms)
 
-    add_rooms = driver.find_element_by_xpath("//a[@aria-label='Add Rooms']")
+    add_rooms = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@aria-label='Add Rooms']")))
     if no_of_rooms > 1:
         for _ in range(no_of_rooms - 1):
             driver.execute_script("arguments[0].click();", add_rooms)
 
-    add_adults = driver.find_element_by_xpath("//a[@aria-label='Add Adults Per Room']")
+    add_adults = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@aria-label='Add Adults Per Room']")))
     if no_of_adults > 1:
         for _ in range(no_of_adults - 1):
             driver.execute_script("arguments[0].click();", add_adults)
 
-    add_children = driver.find_element_by_xpath("//a[@aria-label='Add Children Per Room']")
+    add_children = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@aria-label='Add Children Per Room']")))
     if no_of_children > 0:
         for _ in range(no_of_children):
             driver.execute_script("arguments[0].click();", add_children)
@@ -83,8 +83,7 @@ def scrape_hotels(no_of_rooms, no_of_nights, no_of_adults, no_of_children):
         child_age = driver.find_element_by_xpath("//a[@aria-label='Up Age']")
         child_age.click()
 
-    sleep(2)
-    edit_hotels = driver.find_element_by_xpath("//button[contains(@class,'is-roomkey')]")
+    edit_hotels = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class,'is-roomkey')]")))
     driver.execute_script("arguments[0].click();", edit_hotels)
     logger.info("Finding Hotels as per Requirements....")
 
